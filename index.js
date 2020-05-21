f0189e65e748222716b97c51c3f5391b3071e3d2
Juuso Solonen <m6752@student.jamk.fi>
2020-05-21 14:13:55 +0300

    puhelinluettelon backend step7

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 let persons = [
     {
@@ -27,6 +28,7 @@ const resp = `
 
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
